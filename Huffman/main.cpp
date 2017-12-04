b64a375c8f2bc06671b0eb11c37e017a6d4ad598
钱梦璋 <qmznj1998@163.com>
2017-12-04 08:32:21 +0800

    Initial Commit

---
+++ Huffman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Huffman
+//
+//  Created by QMZ on 2017/12/4.
+//  Copyright © 2017年 QMZ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
